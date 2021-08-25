40e17ccf95b8ba5427f4a268a7a5a911641f559c
JustDylan23 <dylanvdhout@gmail.com>
2021-08-25 11:21:21 +0200

    fixed form types for twig 2

--- Admin/SubmittedFormAdmin.php
+++ Admin/SubmittedFormAdmin.php
@@ -8,23 +8,25 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Sonata\Form\Type\DateTimePickerType;
 
 class SubmittedFormAdmin extends AbstractAdmin
-{    
+{
     protected function configureRoutes(RouteCollection $collection)
     {
         $collection->clearExcept(array('show', 'edit'));
     }
 
     protected function configureFormFields(FormMapper $formMapper)
-    {   
+    {
         $formMapper
-        ->add('createdAt', 'sonata_type_datetime_picker')
-        ->add('submittedValues', 'sonata_type_collection', array(
+        ->add('createdAt', DateTimePickerType::class)
+        ->add('submittedValues', CollectionType::class, array(
             'type_options' => array(
                 // Prevents the "Delete" option from being displayed
                 'delete' => false,
-            )            
+            )
         ), array(
             'edit' => 'inline',
             'inline' => 'table',
